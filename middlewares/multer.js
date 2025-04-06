1fe904455f1296d4fbea624614e9524855e92637
SantoshThapaa <st708611@gmail.com>
2025-04-06 14:38:18 +0545

    Middleware created

---
+++ middlewares/multer.js
@@ -0,0 +1,22 @@
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadPath = 'uploads/';
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const baseName = path.basename(file.originalname, ext);
+    const uniqueName = `${Date.now()}-${baseName}${ext}`;
+    cb(null, uniqueName);
+  }
+});
+const upload = multer({ storage });
+
+export default upload;
